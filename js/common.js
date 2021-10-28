3b410374e6f742dfb686db5f8d5677c3ce05aa10
mkit-5ain <mkit5ain@gmail.com>
2021-10-28 18:08:40 +0900

    [추가] header scroll fadeinout

--- js/common.js
+++ js/common.js
@@ -1,9 +1,23 @@
 $(document).ready(function () {
     imageCache();
+    scrollFadeInOut();
 });
 
 function imageCache () {
     $('img').attr('src',function () {
 		return $(this).attr('src') + "?a=" + Math.random()
 	});
+}
+
+function scrollFadeInOut() {
+    var position = $(window).scrollTop();
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > position) {
+            $('header').addClass('scroll-active');
+        } else {
+            $('header').removeClass('scroll-active');
+        }
+        position = scroll;
+    });
 }
