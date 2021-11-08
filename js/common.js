21e0cf26992b0e9e4669a83b03463c9df0f0b33c
mkit-5ain <mkit5ain@gmail.com>
2021-11-08 14:22:51 +0900

    [수정] js optimization, section control

--- js/common.js
+++ js/common.js
@@ -13,6 +13,8 @@ $(document).ready(function () {
     imageCache();
     scrollFadeInOut();
     menuActive();
+    mouseControl();
+    sectionBg();
 });
 
 function imageCache () {
@@ -37,5 +39,54 @@ function scrollFadeInOut() {
 function menuActive() {
     $('.menu-btn').click(function () {
         $('.menu-view').toggleClass('active');
+    });
+}
+
+function mouseControl () {
+    var cursor = $(".cursor");
+    $(window).mousemove(function(e) {
+        cursor.css({
+            top: e.clientY - cursor.height() / 2,
+            left: e.clientX - cursor.width() / 2
+        });
+    });
+
+    $(window).mouseleave(function() {
+        cursor.css({
+            opacity: "0"
+        });
+    }).mouseenter(function() {
+        cursor.css({
+            opacity: "1"
+        });
+    });
+
+    $(".link").mouseenter(function() {
+        cursor.css({
+            transform: "scale(3.2)"
+        });
+    }).mouseleave(function() {
+        cursor.css({
+            transform: "scale(1)"
+        });
+    });
+
+    $(window).mousedown(function() {
+        cursor.css({
+            transform: "scale(.2)"
+        });
+    }).mouseup(function() {
+        cursor.css({
+            transform: "scale(1)"
+        });
+    });
+}
+
+function sectionBg() {
+    var el = $('.work').offset().top;
+    var scrollHeight = $(document).scroll();
+    $(window).scroll(function () {
+        console.log('body' + el);
+        console.log('스크롤' + scrollHeight);
     });
 }
